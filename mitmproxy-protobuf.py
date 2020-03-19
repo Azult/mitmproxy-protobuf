5243ef232721f029994f1f86d235a4fcad2caec5
iliap <iliap@nsogroup.com>
2020-03-19 20:00:57 +0200

    Update

--- mitmproxy-protobuf.py
+++ mitmproxy-protobuf.py
@@ -37,33 +37,23 @@ def parse_proto(file):
     return parser.safe_call(parser.match_handler("message"), file, root_type) + "\n"
     # exit(1 if len(parser.errors_produced) else 0)
 
-dir_name = "out/" + str(uuid.uuid4())
-mkdir(dir_name)
-mkdir(dir_name + "/requests")
-mkdir(dir_name + "/responses")
-counter = 0
+file_path = 'tmp'
 
 def response(flow: http.HTTPFlow):
     if "nova" in flow.request.pretty_url:
-        global counter
-        counter += 1
-        data = ""
         
         #Parse request
-        file_path = dir_name + "/requests/" + str(counter) + "_" + (flow.request.pretty_url).split("/")[-1]
+        
         with open(file_path,"wb") as f:
             f.write(flow.request.content)
         with open(file_path,"rb") as f:
-            data = parse_proto(f)
-        with open(file_path,"w+") as f:
-            f.write(data)
+            # data = parse_proto(f)
+            flow.request.raw_content = parse_proto(f).encode()
 
         #Parse response
-        file_path = dir_name + "/responses/" + str(counter) + "_" + (flow.request.pretty_url).split("/")[-1]
         with open(file_path,"wb") as f:
             f.write(flow.response.raw_content)
         with open(file_path,"rb") as f:
-            data = parse_proto(f)
-        with open(file_path,"w+") as f:
-            f.write(data)
+            # data = parse_proto(f)
+            flow.response.raw_content = parse_proto(f).encode()
 
